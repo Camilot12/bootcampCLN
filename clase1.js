6b7f34e50f1cc221e44b86588e531ba864480732
Camilot12 <jc.toro12@gmail.com>
2023-11-16 23:17:12 -0500

    cambio drastido en readme.txty nuevos archivos JS

---
+++ clase1.js
@@ -0,0 +1,67 @@
+//Clase de nodejs -- clase de crear un API
+
+//require()
+
+//---------------------------------
+//const os=require('os') 
+//console.log(os.hostname())
+
+//---------------------------------
+//const path = require('path')
+//const filename = 'file1.txt';
+//const fullPath = path.join(__dirname,  filename);
+//console.log('LA NUEVA RUTA ES:', fullPath)
+//console.log(path.dirname(fullPath))
+
+
+
+//---------------------------------
+
+
+const http = require('http');
+
+const PORT = 3000;
+
+
+// request
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+
+    const nombre=req.url.split('?')[0]
+    const edad=req.url.split('?')[1]
+    const bandera=req.url.split('?')[2]
+    //const newBandera = bandera.slice(1)
+    console.log(nombre)
+    console.log(edad)
+    console.log(bandera)
+    const newNombre = nombre.slice(1)
+    console.log(newNombre)
+    const fs=require('fs')
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if (edad >= 18 && bandera == 'si') {
+        console.log('Hola '+newNombre+' eres mayor de edad\n')
+        res.end('Hola '+newNombre+' eres mayor de edad\n');
+        //fs.writeFileSync('edad.txt','Hola '+newNombre+' eres mayor de edad\n', function (){
+        //console.log('Archivo Creado')
+        //})   
+        fs.appendFileSync('edad.txt','Hola '+newNombre+' eres mayor de edad\n')     
+    }
+    else{
+        console.log('Hola '+newNombre+' eres menor de edad\n')
+        res.end('Hola '+newNombre+' eres menor de edad\n');
+        //fs.writeFileSync('edad.txt','Hola '+newNombre+' eres menor de edad\n', function (){
+        //console.log('Archivo Creado')
+        //})
+    }
+    
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found\n');
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
--- readme.txt
+++ readme.txt
@@ -1,7 +1,33 @@
-hola
+000Comunidad-Whatsapp,Discord
+001Videos
+002Textos
+003Temas
+    -JavaScript
+    -Git y Github
+    -Docker
+004
 
-mundo
 
 
-ahora me llamo carlos
+//Pagina statica
 
+------VsCode
+
+https://code.visualstudio.com/
+
+
+------Git
+
+https://www.youtube.com/watch?v=hWglK8nWh60
+https://www.youtube.com/watch?v=PW_A-lOpVV0&list=PLPl81lqbj-4I8i-x2b5_MG58tZfgKmJls
+
+official
+https://git-scm.com/
+
+https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
+
+
+------Docker
+https://www.youtube.com/watch?v=d2pYQmY90tI
+https://www.youtube.com/watch?v=NVvZNmfqg6M
